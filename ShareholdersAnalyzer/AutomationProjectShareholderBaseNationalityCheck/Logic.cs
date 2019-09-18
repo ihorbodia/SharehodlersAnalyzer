815236d885b0c84eb74503f73a7285e0efb1f159
Ihor <ibodia@sii.pl>
2019-09-18 18:01:04 +0200

    Release.

--- ShareholdersAnalyzer/AutomationProjectShareholderBaseNationalityCheck/Logic.cs
+++ ShareholdersAnalyzer/AutomationProjectShareholderBaseNationalityCheck/Logic.cs
@@ -4,14 +4,43 @@ using System;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace AutomationProjectShareholderBaseNationalityCheck
 {
+    public delegate void MessageChangedHandler(string value);
+    public delegate void WorkFinishedHandler();
+    public delegate void WorkStartedHandler();
+
     public class Logic
     {
+        public event MessageChangedHandler MessageChanged;
+        public event WorkFinishedHandler WorkFinished;
+        public event WorkStartedHandler WorkStarted;
         public DirectoryInfo securityFolderPath { get; private set; }
         public FileInfo USADocFilePath { get; private set; }
         public FileInfo USAListFilePath { get; private set; }
+        public string InfoMessage { get; set; }
+
+        private void OnMessageChanged(string value)
+        {
+            var del = MessageChanged as MessageChangedHandler;
+            if (del != null)
+            {
+                InfoMessage = value;
+                del(value);
+            }
+        }
+
+        private void OnWorkStarted()
+        {
+            (WorkStarted as WorkStartedHandler)?.Invoke();
+        }
+
+        private void OnWorkFinished()
+        {
+            (WorkFinished as WorkFinishedHandler)?.Invoke();
+        }
 
         public void GetUSASecurityFilesFolderPath(string str)
         {
@@ -41,60 +70,102 @@ namespace AutomationProjectShareholderBaseNationalityCheck
 			Properties.Settings.Default.Save();
 		}
 
-		public bool CheckIsAppCanStartProcess() => true;
-                //Directory.Exists(securityFolderPath?.FullName) &&
-                //File.Exists(USADocFilePath?.FullName) &&
-                //File.Exists(USAListFilePath?.FullName);
+		public bool CheckIsAppCanStartProcess() =>
+        Directory.Exists(securityFolderPath?.FullName) &&
+        File.Exists(USADocFilePath?.FullName) &&
+        File.Exists(USAListFilePath?.FullName);
 
-		public void StartProcessing()
-		{
-			FileInfo[] Files = securityFolderPath.GetFiles("*.xlsx");
-			foreach (FileInfo file in Files)
-			{
-				string companyName = string.Empty;
-				string fileName = file.Name.Replace(".xlsx", "");
-				string docFileValue = string.Empty;
 
-				using (var stream = File.Open(file.FullName, FileMode.Open, FileAccess.Read))
-				{
-					using (var reader = ExcelReaderFactory.CreateReader(stream))
-					{
-						var result = reader
-							.AsDataSet()
-							.Tables["Feuil1"]
-							.AsEnumerable();
-						if (result.Count() > 1)
-						{
-							companyName = result.Skip(1).First()?.ItemArray[0].ToString();
-						}
-					}
-				}
+        public void RunProcess()
+        {
+            Task.Factory.StartNew(new Action(StartProcessing));
+        }
 
-				using (var stream = File.Open(USADocFilePath.FullName, FileMode.Open, FileAccess.ReadWrite))
-				{
-					using (var reader = ExcelReaderFactory.CreateReader(stream))
-					{
-						var result = reader
-							.AsDataSet()
-							.Tables["Feuil2"]
-							.AsEnumerable();
-						var item = result.FirstOrDefault(x => x.ItemArray[0].ToString().Equals(companyName));
-						docFileValue = item?.ItemArray[5].ToString();
-					}
-				}
+        private void StartProcessing()
+        {
+            OnWorkStarted();
+            OnMessageChanged("Start processing...");
 
-				using (MemoryStream ms = new MemoryStream(File.ReadAllBytes(file.FullName)))
-				{
-					using (ExcelPackage pckg = new ExcelPackage(ms))
-					{
-						var sheet = pckg.Workbook.Worksheets[1];
-						
-						//TODO:
+            FileInfo[] Files = securityFolderPath.GetFiles("*.xlsx");
+            int filesCount = Files.Count();
+            for (int f = 0; f < filesCount; f++)
+            {
+                FileInfo file = Files[f];
+         
+                string companyName = string.Empty;
+                string fileName = file.Name.Replace(".xlsx", "");
+                string docFileValue = string.Empty;
+                
+                try
+                {
+                    using (var stream = File.Open(file.FullName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (var reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            var result = reader
+                                .AsDataSet()
+                                .Tables["Feuil1"]
+                                .AsEnumerable();
+                            if (result.Count() > 1)
+                            {
+                                companyName = result.Skip(1).First()?.ItemArray[0].ToString();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    OnMessageChanged(ex.Message);
+                }
+               
 
-						pckg.Save();
-					}
-				}
-			}
-		}
+                try
+                {
+                    using (var stream = File.Open(USADocFilePath.FullName, FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        using (var reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            var result = reader
+                                .AsDataSet()
+                                .Tables["Feuil2"]
+                                .AsEnumerable();
+                            var item = result.FirstOrDefault(x => x.ItemArray[0].ToString().Equals(companyName));
+                            docFileValue = item?.ItemArray[5].ToString();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    OnMessageChanged(ex.Message);
+                }
+
+
+                try
+                {
+                    using (ExcelPackage pckg = new ExcelPackage(USAListFilePath))
+                    {
+                        var sheet = pckg.Workbook.Worksheets[1];
+                        for (int i = 2; i < sheet.Dimension.Rows + 1; i++)
+                        {
+                            string data = sheet.Cells[$"B{i}"].Text;
+                            if (data.Equals(fileName))
+                            {
+                                sheet.Cells[$"Z{i}"].Value = docFileValue;
+                                break;
+                            }
+                        }
+                        pckg.Save();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    OnMessageChanged(ex.Message);
+                }
+
+                OnMessageChanged($"Processed: {f + 1}/{filesCount + 1}");
+            }
+            OnMessageChanged("Finished");
+            OnWorkFinished();
+        }
     }
 }
--- ShareholdersAnalyzer/AutomationProjectShareholderBaseNationalityCheck/MainForm.cs
+++ ShareholdersAnalyzer/AutomationProjectShareholderBaseNationalityCheck/MainForm.cs
@@ -2,7 +2,10 @@
 
 namespace AutomationProjectShareholderBaseNationalityCheck
 {
-	public partial class MainForm : Form
+    public delegate void UpdateProgressDelegate(string value);
+    public delegate void WorkStarted();
+    public delegate void WorkFinished();
+    public partial class MainForm : Form
 	{
 		Logic logic;
 		public MainForm()
@@ -12,9 +15,58 @@ namespace AutomationProjectShareholderBaseNationalityCheck
 			this.SecurityFolderPathLabel.Text = Properties.Settings.Default.USAFolderPath;
 			this.USADocFilePathLabel.Text = Properties.Settings.Default.USADocFilePath;
 			this.USAListFilePathLabel.Text = Properties.Settings.Default.USAListFilePath;
+
+            logic.MessageChanged += UpdateLabel;
+            logic.WorkFinished += WorkFinished;
+            logic.WorkStarted += WorkStarted;
 		}
 
-		private void SelectSecurityFilesFolderButton_Click(object sender, System.EventArgs e)
+        private void WorkStarted()
+        {
+            if (this.InvokeRequired)
+            {
+                var progrDel = new WorkStarted(WorkStarted);
+                this.BeginInvoke(progrDel);
+            }
+            else
+            {
+                this.SelectUSADocFileButton.Enabled = false;
+                this.SelectSecurityFilesFolderButton.Enabled = false;
+                this.SelectUSAListFileButton.Enabled = false;
+                this.StartButton.Enabled = false;
+            }
+        }
+
+        private void WorkFinished()
+        {
+            if (this.InvokeRequired)
+            {
+                var progrDel = new WorkFinished(WorkFinished);
+                this.BeginInvoke(progrDel);
+            }
+            else
+            {
+                this.SelectUSADocFileButton.Enabled = true;
+                this.SelectSecurityFilesFolderButton.Enabled = true;
+                this.SelectUSAListFileButton.Enabled = true;
+                this.StartButton.Enabled = true;
+            }
+        }
+
+        private void UpdateLabel(string value)
+        {
+            if(this.StatusLabel.InvokeRequired)
+            {
+                var progrDel = new UpdateProgressDelegate(UpdateLabel);
+                this.BeginInvoke(progrDel, value);
+            }
+            else
+            {
+                this.StatusLabel.Text = value;
+            }
+        }
+
+        private void SelectSecurityFilesFolderButton_Click(object sender, System.EventArgs e)
 		{
 			logic.GetUSASecurityFilesFolderPath(null);
 			this.SecurityFolderPathLabel.Text = logic.securityFolderPath.ToString();
@@ -40,9 +92,7 @@ namespace AutomationProjectShareholderBaseNationalityCheck
 			logic.GetUSASecurityFilesFolderPath(Properties.Settings.Default.USAFolderPath);
 			logic.GetUSADocFilePath(Properties.Settings.Default.USADocFilePath);
 			logic.GetUSAListFilesPath(Properties.Settings.Default.USAListFilePath);
-			this.StatusLabel.Text = "Processing...";
-            logic.StartProcessing();
-            this.StatusLabel.Text = "Finished";
+            logic.RunProcess();
         }
 	}
 }
